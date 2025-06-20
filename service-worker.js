0d4625b9bfe8ba032cf684ffce95cd8ee9c76368
dreuscollins <drewskyle4@gmail.com>
2025-06-20 20:06:46 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,20 @@
-self.addEventListener('install', (e) => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('budget-cache').then((cache) => {
-      return cache.addAll(['./', './index.html', './script.js', './styles.css', './logo.png']);
+    caches.open("budget-tracker-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "styles.css",
+        "script.js",
+        "logo.png",
+        "manifest.json"
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
