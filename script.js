c7727c195af061d58cb58772f8c808e671d3de64
dreuscollins <drewskyle4@gmail.com>
2025-06-20 19:51:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -221,4 +221,11 @@ document.addEventListener("DOMContentLoaded", function () {
     sortEntries();
     renderAllEntries();
   });
+
+  if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker registered'))
+    .catch((err) => console.error('Service Worker registration failed:', err));
+}
+
 });
